e761898c3db0db6d8ed6d2a6f6d0374629856582
Bobmarshwilly <danilaloginov5@gmail.com>
2025-04-05 15:15:31 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+import argparse
+from database import engine, SessionLocal
+from models import Base, Note
+
+
+# Функция добавления заметок
+def add_note(content: str):
+    db = SessionLocal()
+    new_note = Note(content=content)
+    db.add(new_note)
+    db.commit()
+    db.refresh(new_note)
+    db.close()
+
+
+# Функция вывода всех заметок
+def list_notes():
+    db = SessionLocal()
+    notes = db.query(Note).all()
+    db.close()
+
+    if not notes:
+        print("Нет заметок!")
+    else:
+        for note in notes:
+            print(note.content)
+
+
+# Определение аргументов получаемых из терминала
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Программа для ведения заметок")
+    parser.add_argument(
+        "command",
+        choices=["add_note", "list_notes"],
+        help="Команды для выполнения",
+    )
+    parser.add_argument("note", type=str, nargs="?", help="Текст заметки")
+    return parser.parse_args()
+
+
+def main():
+    Base.metadata.create_all(bind=engine)
+    args = parse_arguments()
+
+    if args.command == "add_note":
+        add_note(args.note)
+
+    if args.command == "list_notes":
+        list_notes()
+
+
+if __name__ == "__main__":
+    main()
---
+++ models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from database import Base
+from datetime import datetime
+
+
+class Note(Base):
+    """
+    Класс таблицы заметок в базе данных
+
+    id - первичный ключ, идентификатор заметки
+    content - содержание заметки
+    created_at - время добавления заметки
+    """
+
+    __tablename__ = "notes"
+
+    id = Column(Integer, primary_key=True)
+    content = Column(String)
+    created_at = Column(DateTime, default=datetime.now())
+
+    def __repr__(self):
+        return f"<Note(id={self.id}, content={self.content})>"
